b92e4821f6351769a106d681f301305790a0ab66
Jihoon-Ryu <ujihun94@naver.com>
2020-11-09 19:42:35 +0100

    .
    .

--- src/App.js
+++ src/App.js
@@ -1,53 +1,31 @@
 import React from 'react'; 
+import PropTypes from "prop-types";
 //import logo from './logo.svg';
 //import './App.css';
 
-
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-  }
-];
-
-function Food({ name, picture }) {
-  return (
-   <div> 
-    <span>I like {name} </span>
-    <img src={picture} />
-   </div> 
-  )
+class App extends React.Component {
+  /*Mount: constructor => render => componentDidMount
+  Updating by setState => render => componentDidUpdate 
+  */ 
+  state = {
+   isLoading: true,
+   movies: []
+  };
+  
+  render() {
+    const { isLoading } = this.state;
+    return (
+     <div>
+       {isLoading ? "Loading": "We are ready"}
+     </div>
+    )}
+  
+  componentDidMount() {
+    setTimeout(()=> {
+      this.setState({isLoading:false})
+    }, 5000)
+  }  
 }
 
-function App() {
-  return (
-  <div>
-     {foodILike.map(foodILike_ele_obj => (
-     <Food name={foodILike_ele_obj.name} picture={foodILike_ele_obj.image}/>
-     ))}
-  </div>
-  ) 
-}
 
 export default App;
